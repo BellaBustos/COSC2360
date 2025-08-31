df52fd52532e043d1fd86f84063620d5b8cd86fc
BellaBustos <bellabustos@westernwyoming.edu>
2025-08-31 16:47:27 -0600

    Add files via upload

---
+++ project01-02_txt.js
@@ -0,0 +1,14 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: Bella Bustos
+    Date:   8/31/25
+
+    Filename: project01-02.js
+*/let service1Name = "Basic", service2Name = "Express", service3Name = "Extreme", service4Name = "Ultimate";
+
+let service1Speed = "50 Mbps", service2Speed = "100 Mbps", service3Speed = "500 Mbps", service4Speed = "1 Gig";
+
+
+//define variables for service name and service speed
