d3605675bb2f814eb71f62bf8261c7931307b2e5
BellaBustos <bellabustos@westernwyoming.edu>
2025-09-14 19:51:18 -0600

    Add files via upload

---
+++ COSC2360/Module01/project01/project02/project01-02.js
@@ -0,0 +1,22 @@
+/*    JavaScript 7th Edition
+      Chapter 1
+      Hands-on Project 1-2
+
+      Author: Bella Bustos
+      Date: 9/14/2025
+
+      Filename: project01-02.js
+*/
+
+// Declare variables for service names
+let service1Name = "Basic";
+let service2Name = "Express";
+let service3Name = "Extreme";
+let service4Name = "Ultimate";
+
+// Declare variables for service speeds
+let service1Speed = "50 Mbps";
+let service2Speed = "100 Mbps";
+let service3Speed = "500 Mbps";
+let service4Speed = "1 Gig";
+
